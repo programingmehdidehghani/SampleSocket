4fd95efff087c3b08fab9ea4572986d2a5f662b1
mehdi <dehghani_m@rocketmail.com>
2023-03-12 15:41:40 +0330

    add library socket io

--- app/src/main/java/com/example/smaplesocketcrypto/presention/MainActivity.kt
+++ app/src/main/java/com/example/smaplesocketcrypto/presention/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.smaplesocketcrypto
+package com.example.smaplesocketcrypto.presention
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.smaplesocketcrypto.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
