bfc6728d0f887c58ff2ceb0298dfcc8ab4670857
mehdi <dehghani_m@rocketmail.com>
2023-03-15 21:54:42 +0330

    start make view

--- app/src/main/java/com/example/smaplesocketcrypto/presention/MinViewModel.kt
+++ app/src/main/java/com/example/smaplesocketcrypto/presention/MinViewModel.kt
@@ -1,19 +1,42 @@
 package com.example.smaplesocketcrypto.presention
 
 import androidx.lifecycle.ViewModel
+import com.example.smaplesocketcrypto.domain.repository.ExampleRepository
 import dagger.hilt.android.lifecycle.HiltViewModel
+import io.reactivex.rxjava3.subscribers.DisposableSubscriber
 import javax.inject.Inject
 
 @HiltViewModel
 class MinViewModel  @Inject constructor (
-    private val cryptoRepository: CryptoRepository
+    private val exampleRepository: ExampleRepository
 ) : ViewModel() {
 
     init {
-        getConnect()
+        getStartSocket()
     }
 
-    fun getConnect() {
-        cryptoRepository.connect()
+    private fun getStartSocket() {
+        exampleRepository.connect(javaClass.name, object : DisposableSubscriber<ExampleModels>() {
+            override fun onNext(t: ExampleModels) {
+                when (t) {
+                    is ExampleModels.ModelA -> {
+                        mainView.showAData(listOf(t))
+                    }
+                    is ExampleModels.ModelB -> {
+                        mainView.showBData(listOf(t))
+                    }
+                }
+            }
+
+            override fun onComplete() {
+                /* explicitly empty */
+            }
+
+            override fun onError(t: Throwable?) {
+                t?.printStackTrace()
+            }
+        })
     }
+
+
 }
